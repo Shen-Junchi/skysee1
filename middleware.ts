456604f2a9a99c0f2e73e80791c21c046e82b57a
qj20020222 <jian.qi@gwmail.gwu.edu>
2025-01-24 14:42:39 -0500

    12

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
-const isPublicRoute = createRouteMatcher(['/'])
+const isPublicRoute = createRouteMatcher(["/"])
 
 export default clerkMiddleware( async (auth, request) => {
   if (!isPublicRoute(request)) {
